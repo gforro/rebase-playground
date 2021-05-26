3e21e4d32114e8b2ac7d3c800cdb274a238566da
Gabriel Forro <gabriel.forro@gmail.com>
2021-05-26 23:50:37 +0200

    better test desc

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders learn react link in a better way', () => {
   render(<App />);
   // will be conflict
   const linkElement = screen.getByText(/learn react in a/i);
