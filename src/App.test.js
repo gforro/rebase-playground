bdcb38502c5b32eb0be4db91fb338302d7f36d99
Gabriel Forro <gabriel.forro@gmail.com>
2021-05-26 23:47:43 +0200

    add comment

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
+  // will be conflict
   const linkElement = screen.getByText(/learn react in a/i);
   expect(linkElement).toBeInTheDocument();
 });
